d77873da813b3c8fb57ba8e3e1751981bc9aea89
sm093615@gmail.com <sm093615@gmail.com>
2026-07-29 21:28:55 +0530

    mongodb setting

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,9 @@ import express from 'express';
 
 const app = express();  
 
-
+app.get("/", (req, res) =>   {
+    res.send("server is ready 123 ");
+});
 app.listen(5000, () => {
     
     console.log('Server is running on port 5000');  
