2772ffb1dfad882d0ab828b23a523c1168fd1d9d
sm093615@gmail.com <sm093615@gmail.com>
2026-07-29 22:16:19 +0530

    Set MongoDB

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,15 @@
 import express from 'express';  
-
+import dotenv from 'dotenv';   
+import { connectDB } from './config/db.js';
+dotenv.config();     
 const app = express();  
 
 app.get("/", (req, res) =>   {
     res.send("server is ready 123 ");
 });
+// console.log(process.env.MONGO_URI);
 app.listen(5000, () => {
-    
+    connectDB();
     console.log('Server is running on port 5000');  
    
 
