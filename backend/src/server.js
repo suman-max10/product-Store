2cf0fb25d35fe6edcce92be750bbf568980cbf83
sm093615@gmail.com <sm093615@gmail.com>
2026-07-29 22:19:38 +0530

    Clean server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,7 +7,7 @@ const app = express();
 app.get("/", (req, res) =>   {
     res.send("server is ready 123 ");
 });
-// console.log(process.env.MONGO_URI);
+
 app.listen(5000, () => {
     connectDB();
     console.log('Server is running on port 5000');  
