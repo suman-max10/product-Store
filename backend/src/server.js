198f3b3a55bc18974c7ff8dcc9a258a402b5726b
sm093615@gmail.com <sm093615@gmail.com>
2026-07-30 14:25:15 +0530

    create product/create API and test it with POSTMAN

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,31 @@
-import express from 'express';  
-import dotenv from 'dotenv';   
-import { connectDB } from './config/db.js';
-dotenv.config();     
-const app = express();  
+/** @format */
 
-app.get("/", (req, res) =>   {
-    res.send("server is ready 123 ");
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+import Product from "./models/product.model.js";
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+
+app.post("/api/products", async (req, res) => {
+  const products = req.body;
+  if (!products.name || !products.price || !products.image) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Please provide all required fields" });
+  }
+  try {
+    const product = await Product.create(products);
+    res.status(201).json({ success: true, data: product });
+  } catch (error) {
+    console.error(" Error in creating product:", error.message);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
 });
 
 app.listen(5000, () => {
-    connectDB();
-    console.log('Server is running on port 5000');  
-   
-
-    });
+  connectDB();
+  console.log("Server is running on port 5000");
+});
