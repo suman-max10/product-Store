7a4b9609cdcfdabd9d1c360b7528a2b080d0591d
sm093615@gmail.com <sm093615@gmail.com>
2026-07-31 21:47:04 +0530

    Frontend Start

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,20 +1,18 @@
-import { StrictMode } from 'react'
-import { ChakraProvider } from '@chakra-ui/react'
+/** @format */
 
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
+import { createRoot } from "react-dom/client";
 
+import App from "./App.jsx";
 
-
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-     <ChakraProvider >
-      <App />
+    <ChakraProvider>
+      <BroWserRouter>
+        <App />
+      </BroWserRouter>
     </ChakraProvider>
   </StrictMode>,
-)
-
-
-
-
+);
