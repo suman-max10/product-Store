09fe9629042794eb8d177d6608fa11032f3f69e2
sm093615@gmail.com <sm093615@gmail.com>
2026-07-31 11:40:30 +0530

    write and set update a product and handle all edge casaes and test with POSTMAN API

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@
 
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 import { connectDB } from "./config/db.js";
 import Product from "./models/product.model.js";
 dotenv.config();
@@ -49,9 +50,31 @@ app.delete("/api/products/:id", async (req, res) => {
   }
 });
 
+app.put("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const product = req.body;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res
+      .status(404)
+      .json({ success: false, message: "Invalid product ID" });
+  }
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(id, product, {
+      new: true,
+    });
 
+    if (!updatedProduct) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Product not found" });
+    }
 
-
+    res.status(200).json({ success: true, data: updatedProduct });
+  } catch (error) {
+    console.error("Error in updating product:", error.message);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+});
 
 app.listen(5000, () => {
   connectDB();
