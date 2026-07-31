ba636a74474918fdf033e5a0c02484fc1986d1a2
sm093615@gmail.com <sm093615@gmail.com>
2026-07-31 11:23:13 +0530

    write and set delete a product and handle all edge casaes and test with POSTMAN API

--- backend/src/server.js
+++ backend/src/server.js
@@ -35,6 +35,20 @@ app.post("/api/products", async (req, res) => {
   }
 });
 
+app.delete("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    await Product.findByIdAndDelete(id);
+    res
+      .status(200)
+      .json({ success: true, message: "Product deleted successfully" });
+  } catch (error) {
+    console.error("Error in deleting product:", error.message);
+    res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+});
+
 
 
 
