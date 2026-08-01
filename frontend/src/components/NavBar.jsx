c62e78efcba7d0be0ad54fc9e500a4bb709a6407
sm093615@gmail.com <sm093615@gmail.com>
2026-08-01 13:58:48 +0530

    init

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,22 +1,38 @@
-import React from 'react'
-import {container} from '@chakra-ui/react'  
+/** @format */
+
+import React from "react";
+import { Container, Flex, HStack, Text, Button } from "@chakra-ui/react";
+import { Link as RouterLink } from "react-router-dom";
+import { CiSquarePlus } from "react-icons/ci";
+
+
 const NavBar = () => {
   return (
-    <container maxW={"1140px"} px={4}>
-        <Flex h={16}
+    <Container maxW={"1140px"} px={4}>
+      <Flex
+        h={16}
         alignItems={"center"}
         justifyContent={"space-between"}
-        flexDir={{
-          base:"column",  
-          sm:"row"  
-        }}>
+        flexDir={{ base: "column", sm: "row" }}>
+        <Text
+          as={RouterLink}
+          to={"/"}
+          fontSize={{ base: "22px", sm: "28px" }}
+          fontWeight={"bold"}
+          textTransform={"uppercase"}
+          textAlign='center'
+          bgGradient='linear(to-r, cyan.400, blue.500)'
+          bgClip={"text"}>
+          Product Store 🛒
+        </Text>
+        <HStack spacing={2} alignItems={"center"}>
+          <Button as={RouterLink} to={"/create"}>
+           <CiSquarePlus />
+          </Button>
+        </HStack>
+      </Flex>
+    </Container>
+  );
+};
 
-        <Text></Text>
-
-
-        </Flex>
-    </container>
-  )
-}
-
-export default NavBar
+export default NavBar;
